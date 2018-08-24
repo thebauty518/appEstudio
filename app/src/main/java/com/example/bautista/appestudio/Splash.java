a9f3184f31b834a5e74b1ded8147febb83ab5993
Bautista <jabautista065@misena.edu.co>
2018-08-24 17:00:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bautista/appestudio/Splash.java
@@ -0,0 +1,25 @@
+package com.example.bautista.appestudio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(Splash.this, Menu_Principal.class);
+                startActivity(splash);
+                finish();
+
+            }
+        },2000);
+    }
+}
